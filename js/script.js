bbd903722d84fb86d8ff828c6e55812fcceea53e
Joe Hobson <joseph.j.hobson@googlemail.com>
2016-01-10 13:56:16 +0000

    Refactor JS

--- js/script.js
+++ js/script.js
@@ -1,64 +1,95 @@
-$window = $(window);
-if( $window.width() > 800){
-  $('a[href^="#"]').on('click', function() {
-  $('#work').velocity('scroll', { duration: 600 });
-});
-}
+"use strict";
 
-// if( $window.width() > 800){
-//   $(document).ready(function(){
-//     $('body').css('overflow','auto');
-//     $('.overlay').fadeOut();
-//   });
-// }
+var WorkTile = (function() {
 
-function closeWork(){
-  $('.info').find('p').removeClass('reveal');
-  $('.info').find('.before').removeClass('reveal');
-  $('.info').find('ul').removeClass('reveal');
-  $('.closeBtn').css("display","none");
-  setTimeout(function(){
-    $('.slate').removeClass('openWork loading');
-    $('html').removeClass('stopScroll');
-  }, 370);
-  setTimeout(function() {
-    $('.slate').velocity({ height: "290px" }, { queue: false });
-    $('#work').velocity("scroll", { duration: 600, offset: -0}, { queue: false });
-  }, 370);
-}
+  function dataJS(selector) {
+    return document.querySelectorAll("[data-js=" + selector + "]");
+  }
 
-function openWork(){
-  var $dias = $(this);
-  var pos = $(this).position();
-  var w = $( window ).height();
-  $dias.hasClass('openWork')|| $dias.velocity("scroll", { duration:800, offset:-290, easing:"easeInOutQuint"});
-  setTimeout(function() {
-    $dias.velocity("scroll", { duration: 600, offset: -0}, { queue: false });
+  function toggleState($elem, one, two) {
+    $elem.attr("data-state", $elem.attr("data-state") == one ? two : one);
+
+    var state = $elem.attr("data-state");
+
+    if(state === one) {
+      openTile($elem);
+    } else {
+      closeTile($elem);
+    }
+  }
+
+  function openTile($elem) {
+    var w = $(window).height();
+    $elem.velocity("scroll", { duration:800, offset:-290, easing:"easeInOutQuint"});
+
+    setTimeout(function() {
+      $elem.velocity("scroll", { duration: 600, offset: -0}, { queue: false });
+      if ($(window).width() > 768) {
+        $elem.velocity({ height: w }, { queue: false });
+      } else {
+        $elem.velocity({ height: "1240px" }, { queue: false });
+      }
+    }, 870);
+
+    setTimeout(function() {
+      $elem.addClass("openWork loading");
+    }, 1250);
+
+    setTimeout(function() {
+      $elem.find(".info").find("p").toggleClass("reveal");
+      $elem.find(".info").find(".before").toggleClass("reveal");
+      $elem.find(".info").find("ul").addClass("reveal");
+    }, 1600);
+
+    $(".out").on("click", function(evt) {
+      evt.stopPropagation();
+    });
+
     if ($(window).width() > 768) {
-      $dias.velocity({ height: w }, { queue: false });
-  } else {
-      $dias.velocity({ height: "1240px" }, { queue: false });
+      $("html").addClass("stopScroll");
+    }
   }
-  }, 870);
-  if ($(window).width() > 768) {
-    $('html').addClass('stopScroll');
+
+  function closeTile($elem) {
+    $elem.find(".info").find("p").toggleClass("reveal");
+    $elem.find(".info").find(".before").toggleClass("reveal");
+    $elem.find(".info").find("ul").toggleClass("reveal");
+
+    setTimeout(function(){
+      $elem.removeClass("openWork loading");
+      $("html").removeClass("stopScroll");
+    }, 370);
+
+    setTimeout(function() {
+      $elem.velocity({ height: "290px" }, { queue: false });
+      $("#work").velocity("scroll", { duration: 600, offset: -0}, { queue: false });
+    }, 370);
   }
-  setTimeout(function() {
-    $dias.addClass('openWork loading');
 
-  }, 1250);
-  setTimeout(function(e) {
-    $('.info').find('p').addClass('reveal');
-    $('.info').find('.before').addClass('reveal');
-    $('.info').find('ul').addClass('reveal');
-    $('.closeBtn').css("display","inline-block").css("top", (pos.top +20) + "px");
-  }, 1600);
-}
+  function init(attr) {
+    var elem = dataJS(attr);
+    var select = function() {
+      var $slate = $(this);
+      toggleState($slate, "open", "closed");
+    };
 
-$('.slate').on('click', openWork);
+    for (var i = 0; i < elem.length; i++) {
+      var self = elem[i];
+      self.onclick = select;
+    }
+  }
 
-$('body').on('click','.closeBtn', function(e){
-  e.preventDefault();
-  e.stopPropagation();
-  closeWork();
+  return {
+    init: init
+  };
+})();
+
+$(document).ready(function(){
+  WorkTile.init("slate");
+  var $window = $(window);
+  if($window.width() > 800){
+    $("a[href^=\"#\"]").on("click", function() {
+      $("#work").velocity("scroll", { duration: 600 });
+    });
+  }
 });
